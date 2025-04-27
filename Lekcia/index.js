0f046c84f580fe92d58275168b5a043a4c0353e4
nursultanzhh <nursultan.zhumamuratov@gmail.com>
2025-04-27 05:18:04 +0500

    adapted for mobile version

--- Lekcia/index.js
+++
@@ -1,7 +0,0 @@
-// const btn= document.querySelector('#btn');
-// btn.onclick= function(){remove.remove() ;}
-
-
-btn.addEventListener("click", () => alert("1"));
-btn.removeEventListener("click", () => alert("1"));
-btn.onclick = () => alert(2);
